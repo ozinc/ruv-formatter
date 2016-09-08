55b8eef7618ceba1a9412a59c34d140e7672a696
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-09-08 16:16:40 +0100

    Test for first event is here

--- test/epg.js
+++ test/epg.js
@@ -6,20 +6,19 @@ const epg = require('../lib/epg');
 const cheerio = require('cheerio');
 
 // Read in test data
-const $ = cheerio.load(fs.readFileSync('etc/test-data.xml'));
+const $ = cheerio.load(fs.readFileSync('etc/test-data.xml'), { xmlMode: true });
 
 describe('EPG formatting', () => {
 
   it('should correctly parse a simple video without a collection', () => {
     let video = $('event').get(0);
     let res = epg.formatEvent($, video);
-    expect(res).to.equal({
+    expect(res).to.deep.equal({
       "type": "video",
       "id": "4858083",
       "start-time": "2016-09-08 11:45:33",
       "end-time": "2016-09-08 15:36:53",
       "title": "ÓL fatlaðra 2016: Setningarathöfn",
-      "category": "sports",
       "description": "Upptaka frá setningarathöfn Ólympíumóts fatlaðra í Ríó.",
       "image": "http://dagskra.servefir.ruv.is/kringlumyndir/18199/18199-1.jpg",
       "vod": false
