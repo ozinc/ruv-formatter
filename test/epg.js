7c9ce6bcafe8b88052cf6bff726d38eeeaeb54fb
Hrafn Eiriksson <hrafn@eiriksson.is>
2016-09-08 21:47:56 +0100

    TDD

--- test/epg.js
+++ test/epg.js
@@ -25,4 +25,27 @@ describe('EPG formatting', () => {
     });
   });
 
+  it('should correctly parse a video with a collection', () => {
+    let res = epg.formatEvent($, $('event').get(1));
+    expect(res).to.deep.equal({
+      "type": "video",
+      "id": "4857497",
+      "start-time": "2016-09-08 16:20:00",
+      "end-time": "2016-09-08 17:05:41",
+      "title": "Violetta",
+      "category": "children",
+      "description": "Disneyþáttaröð fyrir börn og unglinga um hina hæfileikaríku Violettu, sem snýr aftur heim til Buenos Aires eftir að hafa búið um tíma í Evrópu. Aðalhlutverk: Diego Ramos, Martina Stoessel og Jorge Blanco.",
+      "image": "http://dagskra.servefir.ruv.is/kringlumyndir/18329/18329-1.jpg",
+      "vod": "2016-09-15",
+      "collection": {
+        "id": "18329",
+        "title": "Violetta",
+        "type": "series",
+        "description": "Disneyþáttaröð um hina hæfileikaríku Violettu, sem snýr aftur til heimalands síns, Buenos Aires eftir að hafa búið um tíma í Evrópu. Aðalhlutverk: Diego Ramos, Martina Stoessel og Jorge Blanco.",
+        "season-number": 1,
+        "episode-number": 26
+      }
+    });
+  });
+
 });
